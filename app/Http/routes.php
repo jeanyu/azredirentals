afd70ce22d43be5d04ce90ed4ad38868661f4231
Nat <nat@mybizniche.com>
2015-10-22 13:14:12 +0800

    boostrap setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('test',['as'=>'admin.page.index','uses'=>'Admin\PageController@index']);
---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Admin</title>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo URL::to('/');?>/library/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+    @yield('content')
+       <!-- Site footer -->
+      <footer class="footer">
+        <p>&copy; Company 2014</p>
+      </footer>
+
+    </div> <!-- /container -->
+    <script src="<?php echo URL::to('/');?>/library/jquery/jquery.min.js"></script>
+    <script src="<?php echo URL::to('/');?>/library/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
